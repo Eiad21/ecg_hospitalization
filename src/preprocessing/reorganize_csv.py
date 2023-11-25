8f68c7e074387cdf3675e2bd33b90f902d500dfa
Eiad.Tantawy <eiadessam1@gmail.com>
2023-11-25 22:47:04 +0100

    update csv reorg

--- src/preprocessing/reorganize_csv.py
+++ src/preprocessing/reorganize_csv.py
@@ -6,13 +6,21 @@ def reorganize_data():
     df = pd.read_csv(csv_path, delimiter=';')
 
     # Combine EntryDate and EntryTime to one datetime column
-    df['datetime'] = pd.to_datetime(df['EntryDate'] + ' ' + df['EntryTime'], format='%d.%m.%Y %H:%M:%S')
+    df['entry_datetime'] = pd.to_datetime(df['EntryDate'] + ' ' + df['EntryTime'], format='%d.%m.%Y %H:%M:%S')
+    end_datetime = pd.to_datetime(df['EndDate'] + ' ' + df['EndTime'], format='%d.%m.%Y %H:%M:%S')
+    df['time_spent'] = (end_datetime - df['entry_datetime']).dt.total_seconds() / (24.0 * 60 * 60)
 
     # Drop the original date and time columns
-    df = df.drop(['EntryDate', 'EntryTime'], axis=1)
+    df = df.drop(['EntryDate', 'EntryTime', 'EndDate', 'EndTime'], axis=1)
+
+    # Drop columns with majority of na values. 15928 and 16537 respectively
+    df = df.drop(['AdmissionRZP', 'DischargeTo_unit_agg'], axis=1)
+
+    # Drop rows where DgCode is na (11 entries)
+    df['DgCode'] = df['DgCode'].fillna('unknown')
 
     # Specify the order of columns for sorting
-    sort_order = ['patient_ID', 'visit_ID', 'datetime']
+    sort_order = ['patient_ID', 'visit_ID', 'entry_datetime']
 
     # Sort the DataFrame based on the specified columns
     df = df.sort_values(by=sort_order)
