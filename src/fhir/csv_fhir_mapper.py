b31c79efbf53987c6e3d796231a4fc7cbe87f17f
Eiad.Tantawy <eiadessam1@gmail.com>
2023-11-25 22:36:54 +0100

    update fhir

--- src/fhir/csv_fhir_mapper.py
+++ src/fhir/csv_fhir_mapper.py
@@ -6,17 +6,25 @@ from fhirclient import client
 
 import pandas as pd
 
-def convert_csv_to_patients(csv_file):
+
+def convert_csv_to_patients():
     processed_meta_path = '../../data/processed/meta/IKEM_processed.csv'
 
-    df = pd.read_csv(processed_meta_path, delimiter=';')
+    df = pd.read_csv(processed_meta_path)
     # Read CSV and convert to FHIR Patient resources
     patients = []
     # Your CSV reading and conversion logic here...
-    for row in df:
+    for i, row in df.iterrows():
         patient = Patient()
         patient.identifier = [Identifier(value=row['patient_ID'])]
         patient.birthDate = row['age']
+
+        print(patient)
+        break
         # Map other columns to corresponding fields in Patient resource
         patients.append(patient)
     return patients
+
+
+if __name__ == '__main__':
+    convert_csv_to_patients()
