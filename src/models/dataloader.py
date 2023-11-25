d1d78b10113c18153893bee90e54a5d95258c64e
Abdulkader Ghandoura <68954705+abdulkaderghandoura@users.noreply.github.com>
2023-11-25 11:35:16 +0100

    Dataloader modification

--- src/models/dataloader.py
+++ src/models/dataloader.py
@@ -1,24 +1,53 @@
 import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
+from pathlib import Path
+import sys
+import pickle
+sys.path.append('../utils/')
+from ecg_processing import ECGXMLReader
 
 class PatientDataset(Dataset):
-    def __init__(self, csv_file):
-        self.data = pd.read_csv(csv_file)
+    def __init__(self, meta_path):
+        self.data = pd.read_csv(meta_path)
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        sample = {
+        meta = {
             'patient_ID': self.data.iloc[idx]['patient_ID'],
             'visit_ID': self.data.iloc[idx]['visit_ID'],
-            'EntryDate': self.data.iloc[idx]['EntryDate'],
-            'age': self.data.iloc[idx]['age'],
-            'discharge_reason': self.data.iloc[idx]['discharge_reason'],
+            'DischargeTo_Agg': self.data.iloc[idx]['DischargeTo_Agg'],
             'file': self.data.iloc[idx]['file'],
         }
 
-        # You can add more processing or transformations here if needed
+        try:
+            raw_data_dir = Path('../../data/raw/xml')
+            xml_file_path = raw_data_dir / meta['file']
+            ecg_reader = ECGXMLReader(xml_file_path)
+            ecg_data = ecg_reader.getAllVoltages()
+        except Exception as e:
+            print(f"Error in the ECG XML Reader: {str(e)}")
 
-        return sample
+        return ecg_data, meta
+
+if __name__ == "__main__":
+    processed_meta_path = Path('../../data/processed/meta/IKEM_processed.csv')
+    patient_dataset = PatientDataset(processed_meta_path)
+    print(patient_dataset[0][0])
+
+    # batch_of_data = list()
+    # for i in range(10):
+        
+    #     batch_of_data.append(patient_dataset[i + 1][0])
+
+    # Specify the file path where you want to save the data
+    # batch_of_data_path = 'batch_of_data.pickle'
+
+    # # Open the file in binary write mode
+    # with open(batch_of_data_path, 'wb') as file:
+    #     # Dump the data into the file using pickle
+    #     pickle.dump(batch_of_data, file)
+
+    # print(f"Data saved to {batch_of_data_path}")
